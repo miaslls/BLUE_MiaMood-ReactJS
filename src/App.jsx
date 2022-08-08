76451fdff8509fc961987941de40f24b0eeff89d
miaslls <97785569+miaslls@users.noreply.github.com>
2022-08-08 20:42:31 -0300

    added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
+import Header from "./components/Header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <div id="outer-container">
+      <Header />
+      <main>
+        <section id="mood-list"></section>
+        <section id="statistics"></section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <div id="page-title">
+        Mia<span>Mood</span>
+      </div>
+      <nav>
+        <div className="nav-icon">F</div>
+        <div className="nav-icon">D</div>
+        <div className="nav-icon">(</div>
+        <div className="nav-icon">¯</div>
+        <div className="nav-icon">2</div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
