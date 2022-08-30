8c7ef2bda267c8692702cdd4ff5fca38d3b01e8e
miaslls <97785569+miaslls@users.noreply.github.com>
2022-08-30 10:56:22 -0300

    getMoodList + Loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,46 @@
 import 'assets/CSS/App.css';
 
 import { useState, useEffect } from 'react';
-// import { MoodService } from 'services/MoodService';
+import { MoodService } from 'services/MoodService';
 // import { getDateToday, getTimeNow } from 'util/getDateTimeNow';
 
 import Header from 'components/Header';
 import MoodList from 'components/MoodList';
+import Loading from 'components/Loading';
 
 const moodIcons = ['<', '*', '2', '.', '"', 'A'];
-
-// 🦋
-const mockMoodList = [
-  {
-    _id: 1,
-    type: 1,
-    text: 'mock mood 1',
-    date: '2022-08-30',
-    time: '09:44:05',
-  },
-  {
-    _id: 2,
-    type: 2,
-    text: 'mock mood 2',
-    date: '2022-08-30',
-    time: '09:45:10',
-  },
-  {
-    _id: 3,
-    type: 3,
-    text: 'mock mood 3',
-    date: '2022-08-29',
-    time: '09:46:15',
-  },
-];
 
 // 📌📌📌 function APP
 
 function App() {
   const [moodList, setMoodList] = useState([]);
-  const [selectedMoodList, setSelectedMoodList] = useState();
+  const [selectedMoodList, setSelectedMoodList] = useState('date');
+  const [moodListLoading, setMoodListLoading] = useState('false');
+
+  // ----- 📌 getMoodList
+
+  const getMoodList = async () => {
+    setMoodListLoading(true);
+
+    let response;
+
+    selectedMoodList === 'date'
+      ? (response = await MoodService.getTodayMoods())
+      : (response = await MoodService.getAllMoods());
+
+    setMoodList(response.moods);
+    setMoodListLoading(false);
+  };
 
   useEffect(() => {
-    setMoodList(mockMoodList);
-    setSelectedMoodList('date');
+    getMoodList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(moodList, selectedMoodList); // 🦋
+  useEffect(() => {
+    getMoodList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedMoodList]);
 
   return (
     <div id="outer-container">
@@ -56,7 +51,11 @@ function App() {
       <main>
         {/* ----- 📌 MOODLIST */}
 
-        <MoodList moodIcons={moodIcons} moodList={moodList} selectedMoodList={selectedMoodList} />
+        {moodListLoading && <Loading />}
+
+        {!moodListLoading && (
+          <MoodList moodIcons={moodIcons} moodList={moodList} selectedMoodList={selectedMoodList} />
+        )}
       </main>
     </div>
   );
